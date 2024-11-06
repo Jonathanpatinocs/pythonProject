e6431af43657fceadc7fa0b3c6635f307fab1f82
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2024-11-06 14:06:41 -0500

    turtle

---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("red")
+tim.forward(100)
+my_screen = Screen()
+my_screen.bgcolor("yellow")
+my_screen.exitonclick()
