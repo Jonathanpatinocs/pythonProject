b113ffd7793664122634e42f65fc89b0ce68786d
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2024-11-06 14:29:22 -0500

    table

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+# from prettytable import PrettyTable
+# # tim = Turtle()
+# # tim.shape("turtle")
+# # tim.color("red")
+# # tim.forward(100)
+# # my_screen = Screen()
+# # my_screen.bgcolor("yellow")
+# # my_screen.exitonclick()
+#
+# table = PrettyTable()
+#
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.align = "l"
+# print(table)
 
-tim = Turtle()
-tim.shape("turtle")
-tim.color("red")
-tim.forward(100)
-my_screen = Screen()
-my_screen.bgcolor("yellow")
-my_screen.exitonclick()
+
+
