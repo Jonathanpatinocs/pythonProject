c05cb0063fc4ed2f8e0208db33d01ae499512c08
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2024-11-07 11:57:34 -0500

    img

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-# from turtle import Turtle, Screen
-# from prettytable import PrettyTable
-# # tim = Turtle()
-# # tim.shape("turtle")
-# # tim.color("red")
-# # tim.forward(100)
-# # my_screen = Screen()
-# # my_screen.bgcolor("yellow")
-# # my_screen.exitonclick()
-#
-# table = PrettyTable()
-#
-# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-# table.align = "l"
-# print(table)
+from turtle import Turtle, Screen
+import
+from colorgram import *
+import random
+turtle = Turtle()
+screen = Screen()
+screen.colormode(255)
+colors = colorgram.extract(dots.webp)
+turtle.speed(10)
 
 
+
+for x in range(int(360)):
+    turtle.pencolor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    turtle.circle(100)
+    turtle.right(turtle.heading() + 10)
+
+
+
+screen.exitonclick()
 
